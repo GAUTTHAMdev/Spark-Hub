5fd06d10d44b77a1a94b03f774b8e9356321d1f8
Gautham.B <VivianGautham1124@gmail.com>
2025-07-04 20:35:30 +0530

    Add starter Discord bot code with requirements

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Logged in as {bot.user}!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong!")
+
+bot.run(TOKEN)
